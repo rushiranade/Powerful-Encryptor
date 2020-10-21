a5a39a4295d50bf8e52721313afe1dc11f06d522
rushiranade <71251507+rushiranade@users.noreply.github.com>
2020-10-21 18:57:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ word = input("Enter the word: ")
 hashamount = int(input("Hash amount. Recommended: 100 :- "))
 n = int(input("Enter a number that indicates how many letters you want to change: "))
 helper = input("Enter a word that acts as the encryption helper: ")
-for i in range(hashamout):
+for i in range(hashamount):
     a, word = word[:n], word[n:]
     word = word + a + encrypt(helper, 4, n) # n-4Shift Encryption
     n = n + 1
