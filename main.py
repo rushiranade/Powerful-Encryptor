6a8e76a6e935a94592b58d665c9b37a9406163b9
rushiranade <71251507+rushiranade@users.noreply.github.com>
2020-10-21 18:58:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ n = int(input("Enter a number that indicates how many letters you want to change
 helper = input("Enter a word that acts as the encryption helper: ")
 for i in range(hashamount):
     a, word = word[:n], word[n:]
-    word = word + a + encrypt(helper, 4, n) # n-4Shift Encryption
+    word = word + a + encryptor(helper, 4, n) # n-4Shift Encryption
     n = n + 1
 print(f"Cipher: {word}")
