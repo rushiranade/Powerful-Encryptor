8eab76af1ee0009d08ee44959da43fe54164ad8d
rushiranade <71251507+rushiranade@users.noreply.github.com>
2020-10-21 18:55:22 +0530

    Create acc.py

---
+++ acc.py
@@ -0,0 +1,19 @@
+# COPYRIGHT 2020 - RUSHI RANADE'S APPS
+# ENHANCED ENCRYPTION
+# CHECK MY OTHER ENCRYPTION REPO
+def encrypt(text,s, n): 
+    result = "" 
+    for i in range(len(text)):
+        char = text[i] 
+  
+        if (char.isupper()): 
+            result += chr((ord(char) + s-65) % 26 + 65) 
+  
+        else: 
+            result += chr((ord(char) + s - 97) % 26 + 97)
+        if (s * n) <=26:
+            s = s * n
+        else:
+            s = (s - n) * n
+  
+    return result
